44947968ae484bbe7ba7aa91ad21a34093c66cbd
Alokesh Ghosh <alokeshghosh.5555@gmail.com>
2026-09-08 22:38:00 +0530

    Support deployment port

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 
@@ -38,5 +39,10 @@ func main() {
 	}
 
 	// Start server
-	r.Run(":8080")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	r.Run(":" + port)
 }
